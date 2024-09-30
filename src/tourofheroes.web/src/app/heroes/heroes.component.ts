1bb1b7f77c0d7afc057b5072d078a27091c83f19
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-30 20:34:17 +0100

    Added display list of heroes

--- src/tourofheroes.web/src/app/heroes/heroes.component.ts
+++ src/tourofheroes.web/src/app/heroes/heroes.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
-import { UpperCasePipe } from '@angular/common';
+import { NgIf, NgFor, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [FormsModule, UpperCasePipe],
+  imports: [FormsModule, NgIf, NgFor, UpperCasePipe],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
