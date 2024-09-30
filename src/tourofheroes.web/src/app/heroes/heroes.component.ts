1a3f3fa44b466e8c2496f7591d1c16e86162fead
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-30 21:10:54 +0100

    Added services

--- src/tourofheroes.web/src/app/heroes/heroes.component.ts
+++ src/tourofheroes.web/src/app/heroes/heroes.component.ts
@@ -3,7 +3,7 @@ import { NgIf, NgFor, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,9 +13,22 @@ import { HEROES } from '../mock-heroes';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   
+  constructor(private heroService: HeroService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
