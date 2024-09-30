d08f24f716e2d9b964b685554a56f199ed3088e6
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-30 21:22:45 +0100

    Added messages

--- src/tourofheroes.web/src/app/heroes/heroes.component.ts
+++ src/tourofheroes.web/src/app/heroes/heroes.component.ts
@@ -1,35 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgIf, NgFor, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
   imports: [FormsModule, HeroDetailComponent, NgIf, NgFor, UpperCasePipe],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
   heroes: Hero[] = [];
   selectedHero?: Hero;
-  
-  constructor(private heroService: HeroService) {
 
-  }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
