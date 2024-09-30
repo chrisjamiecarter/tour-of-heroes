c3ded9b562476b73534ddbb69e588e5de80c7d99
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-30 20:23:07 +0100

    Added hero editor

---
+++ src/tourofheroes.web/src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [FormsModule, UpperCasePipe],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
